443d25cc8094bedeb19b50a3d8d63bc399c068d6
nadine-shi <nadine@afterlogic.com>
2019-04-26 15:11:34 +0300

    License and other texts improving

--- Managers/Mailchimp.php
+++ Managers/Mailchimp.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * @copyright Copyright (c) 2019, Afterlogic Corp.
- * @license AGPL-3.0 or Afterlogic Software License
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
  */
 
 namespace Aurora\Modules\Sales\Managers;
 
 /**
- * This code is licensed under AGPLv3 license or Afterlogic Software License
- * if commercial version of the product was purchased.
- * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
+ * @copyright Copyright (c) 2019, Afterlogic Corp.
  */
 class Mailchimp extends \Aurora\System\Managers\AbstractManager
 {
